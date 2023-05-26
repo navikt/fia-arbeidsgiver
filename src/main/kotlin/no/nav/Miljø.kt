c1c6de94de96bf976eb5154f9295561a1426c330
Christian Klem <christian.klem@nav.no>
2023-05-26 14:49:53 +0200

    Kræsj ved oppstart, dersom man mangler miljø variabler

--- src/main/kotlin/no/nav/Miljø.kt
+++ src/main/kotlin/no/nav/Miljø.kt
@@ -1,11 +1,11 @@
 package no.nav
 
 internal object Miljø {
-    val tokenxClientId: String by lazy { System.getenv("TOKEN_X_CLIENT_ID") }
-    val tokenxIssuer: String by lazy { System.getenv("TOKEN_X_ISSUER") }
-    val tokenxJwkPath: String by lazy { System.getenv("TOKEN_X_JWKS_URI") }
-    val tokenxPrivateJwk: String by lazy { System.getenv("TOKEN_X_PRIVATE_JWK") }
-    val tokenXTokenEndpoint: String by lazy { System.getenv("TOKEN_X_TOKEN_ENDPOINT") }
-    val altinnProxyUrl: String by lazy { System.getenv("ALTINN_RETTIGHETER_PROXY_URL") }
-    val altinnRettigheterProxyClientId: String by lazy { System.getenv("ALTINN_RETTIGHETER_PROXY_CLIENT_ID") }
+    val tokenxClientId: String = System.getenv("TOKEN_X_CLIENT_ID")
+    val tokenxIssuer: String = System.getenv("TOKEN_X_ISSUER")
+    val tokenxJwkPath: String = System.getenv("TOKEN_X_JWKS_URI")
+    val tokenxPrivateJwk: String = System.getenv("TOKEN_X_PRIVATE_JWK")
+    val tokenXTokenEndpoint: String = System.getenv("TOKEN_X_TOKEN_ENDPOINT")
+    val altinnProxyUrl: String = System.getenv("ALTINN_RETTIGHETER_PROXY_URL")
+    val altinnRettigheterProxyClientId: String = System.getenv("ALTINN_RETTIGHETER_PROXY_CLIENT_ID")
 }
