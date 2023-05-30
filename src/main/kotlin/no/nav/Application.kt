ad310b043e3e55ccc03258e5538278f881468336
Christian Klem <christian.klem@nav.no>
2023-05-30 10:56:52 +0200

    Fjern logging av miljøvariebler ved oppstart

--- src/main/kotlin/no/nav/Application.kt
+++ src/main/kotlin/no/nav/Application.kt
@@ -4,15 +4,13 @@ import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import no.nav.plugins.*
-import org.slf4j.LoggerFactory
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
 }
-val logger = LoggerFactory.getLogger("mainLog")
+
 fun Application.module() {
-    logger.info("MILJØ issuer: ${Miljø.tokenxIssuer}, clientId: ${Miljø.tokenxClientId}")
     configureMonitoring()
     configureSerialization()
     configureSecurity()
