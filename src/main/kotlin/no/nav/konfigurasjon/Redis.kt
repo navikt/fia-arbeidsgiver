be3aae50131793b728051802f92722b342b3d5bc
Per-Christian Nielsen <per-christian.nielsen@nav.no>
2023-12-22 09:36:23 +0100

    Prøvebytter til Aiven redis

--- src/main/kotlin/no/nav/konfigurasjon/Redis.kt
+++ src/main/kotlin/no/nav/konfigurasjon/Redis.kt
@@ -2,8 +2,8 @@ package no.nav.konfigurasjon
 
 class Redis {
     companion object {
-        val redisHost: String by lazy { System.getenv("REDIS_HOST") }
-        val redisPort: Int by lazy { System.getenv("REDIS_PORT").toInt() }
-        val redisPassword: String by lazy { System.getenv("REDIS_PASSWORD") }
+        val redisUrl: String by lazy { System.getenv("REDIS_URI_STATUS") }
+        val redisUsername: String by lazy { System.getenv("REDIS_USERNAME_STATUS") }
+        val redisPassword: String by lazy { System.getenv("REDIS_PASSWORD_STATUS") }
     }
 }
