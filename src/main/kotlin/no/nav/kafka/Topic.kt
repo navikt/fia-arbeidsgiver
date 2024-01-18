a1c480f55b856d9db1049d2dcde1dae196f2bca9
Christian Klem <christian.klem@nav.no>
2024-01-18 09:18:36 +0100

    Fiks topic navn
    Co-authored-by: Per-Christian Nielsen <per-christian.nielsen@nav.no>

--- src/main/kotlin/no/nav/kafka/Topic.kt
+++ src/main/kotlin/no/nav/kafka/Topic.kt
@@ -1,8 +1,8 @@
 package no.nav.kafka
 
 enum class Topic(val navn: String, val prefix: String = "pia") {
-    KARTLEGGING_SVAR("kartlegging-svar-topic-v1"),
-    KARTLEGGING_SPØRREUNDERSØKELSE("kartlegging-topic-v1"),
+    KARTLEGGING_SVAR("kartlegging-svar-v1"),
+    KARTLEGGING_SPØRREUNDERSØKELSE("kartlegging-v1"),
     SAK_STATUS("ia-sak-status-v1");
     fun navnMedPrefix() = "${prefix}.${navn}"
 }
