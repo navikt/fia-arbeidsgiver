34cfd2d51e17dd2580187c005e2a6780e1c17b98
Sindre Larsen <silar.dev@proton.me>
2024-03-26 10:07:11 +0100

    Legger til temabeskrivelse i spørsmåloversikt så vi får vist tema tittel
    Co-authored-by: Per-Christian Nielsen <per-christian.nielsen@nav.no>

--- src/main/kotlin/no/nav/fia/arbeidsgiver/sporreundersokelse/api/dto/SpørsmålsoversiktDto.kt
+++ src/main/kotlin/no/nav/fia/arbeidsgiver/sporreundersokelse/api/dto/SpørsmålsoversiktDto.kt
@@ -3,10 +3,12 @@ package no.nav.fia.arbeidsgiver.sporreundersokelse.api.dto
 import kotlinx.serialization.Serializable
 import no.nav.fia.arbeidsgiver.sporreundersokelse.domene.Spørreundersøkelse
 import no.nav.fia.arbeidsgiver.sporreundersokelse.domene.SpørsmålOgSvaralternativer
+import no.nav.fia.arbeidsgiver.sporreundersokelse.domene.temaFraSpørsmålId
 import no.nav.fia.arbeidsgiver.sporreundersokelse.kafka.dto.SvaralternativDto
 
 @Serializable
 data class SpørsmålsoversiktDto(
+    val temabeskrivelse: String,
     val spørsmålTekst: String,
     val svaralternativer: List<SvaralternativDto>,
     val nesteSpørsmål: IdentifiserbartSpørsmål?,
@@ -15,6 +17,7 @@ data class SpørsmålsoversiktDto(
 
 fun SpørsmålOgSvaralternativer.tilSpørsmålsoversiktDto(spørreundersøkelse: Spørreundersøkelse) =
     SpørsmålsoversiktDto(
+        temabeskrivelse = spørreundersøkelse.temaMedSpørsmålOgSvaralternativer.temaFraSpørsmålId(spørsmålId = id).beskrivelse,
         spørsmålTekst = spørsmål,
         svaralternativer = svaralternativer.map {
             SvaralternativDto(
