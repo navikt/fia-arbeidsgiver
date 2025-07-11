876cda407cd81355851b01f636530106878467ea
Thomas Dufourd <thomas.dufourd@gmail.com>
2025-07-11 08:03:45 +0200

    Exchange en self-issued tokenX token (med custom claims) for å kalle fia-dokument-publisering

--- src/main/kotlin/no/nav/fia/arbeidsgiver/http/tokenx/TokenExchanger.kt
+++ src/main/kotlin/no/nav/fia/arbeidsgiver/http/tokenx/TokenExchanger.kt
@@ -18,31 +18,43 @@ import java.util.UUID
 object TokenExchanger {
     private val privateKey = RSAKey.parse(Miljø.tokenxPrivateJwk).toRSAPrivateKey()
 
-    enum class Scope(val value: String) {
-        DOKUMENT_LESETILGANG("read:dokument"),
-        SYKEFRAVARSSTATISTIKK_LESETILGANG("read:sykefravarsstatistikk"),
-        SAMARBEID_LESESTILGANG("read:samarbeid"),
+    // Exchange token for Altinn Tilganger hvor det trengs pid/fnr i claim (kommer fra OpenId token)
+    internal suspend fun exchangeMedOpenIdToken(
+        audience: String,
+        openIdToken: String,
+    ) = Instant.now().let {
+        exchangeToken(
+            subjecToken = openIdToken,
+            audience = audience,
+            now = it,
+        )
     }
 
-    internal suspend fun exchangeToken(
-        token: String,
+    // Exchange token for Fia-dokument-publisering hvor det trengs custom claim "tilgang_fia"
+    internal suspend fun exchangeMedSelfIssuedToken(
         audience: String,
-        scope: Scope? = null,
+        scope: Scope,
+        orgnr: String,
+    ) = Instant.now().let {
+        exchangeToken(
+            subjecToken = createJwt(
+                atInstant = it,
+                customClaim = TilgangClaim(scope = scope, orgnr = orgnr)
+            ),
+            audience = audience,
+            now = it,
+        )
+    }
+
+
+    private suspend fun exchangeToken(
+        subjecToken: String, // Claim-set i subject_token mappes til claim-set i exchange-tokenet
+        audience: String,
+        now: Instant,
     ): String =
         try {
             client.post(URI.create(Miljø.tokenXTokenEndpoint).toURL()) {
-                val now = Instant.now()
-                val clientAssertion = JWT.create().apply {
-                    withSubject(Miljø.tokenxClientId)
-                    withIssuer(Miljø.tokenxClientId)
-                    withAudience(Miljø.tokenXTokenEndpoint)
-                    withJWTId(UUID.randomUUID().toString())
-                    withIssuedAt(Date.from(now))
-                    withNotBefore(Date.from(now))
-                    withExpiresAt(Date.from(now.plusSeconds(120)))
-                    if (scope != null) withClaim("tilgang_fia_ag", scope.value)
-                }.sign(Algorithm.RSA256(null, privateKey))
-
+                val clientAssertion = createJwt(atInstant = now)
                 setBody(
                     FormDataContent(
                         Parameters.build {
@@ -50,7 +62,7 @@ object TokenExchanger {
                             append("client_assertion_type", "urn:ietf:params:oauth:client-assertion-type:jwt-bearer")
                             append("client_assertion", clientAssertion)
                             append("subject_token_type", "urn:ietf:params:oauth:token-type:jwt")
-                            append("subject_token", token)
+                            append("subject_token", subjecToken)
                             append("audience", audience)
                         },
                     ),
@@ -59,4 +71,35 @@ object TokenExchanger {
         } catch (e: Exception) {
             throw RuntimeException("Token exchange feil", e)
         }
+
+    private fun createJwt(
+        atInstant: Instant,
+        customClaim: TilgangClaim? = null,
+    ): String {
+        return JWT.create().apply {
+            withSubject(Miljø.tokenxClientId)
+            withIssuer(Miljø.tokenxClientId)
+            withAudience(Miljø.tokenXTokenEndpoint)
+            withJWTId(UUID.randomUUID().toString())
+            withIssuedAt(Date.from(atInstant))
+            withNotBefore(Date.from(atInstant))
+            withExpiresAt(Date.from(atInstant.plusSeconds(120)))
+            if (customClaim != null) withClaim(customClaim.name(), customClaim.value())
+        }.sign(Algorithm.RSA256(null, privateKey))
+    }
+}
+
+enum class Scope(val value: String) {
+    DOKUMENT_LESETILGANG("read:dokument"),
+    SYKEFRAVARSSTATISTIKK_LESETILGANG("read:sykefravarsstatistikk"),
+    SAMARBEID_LESESTILGANG("read:samarbeid"),
+}
+
+data class TilgangClaim(
+    val orgnr: String,
+    val scope: Scope,
+) {
+    fun name(): String = "tilgang_fia"
+    fun value(): String = "${scope.value}:$orgnr"
+    override fun toString(): String = value()
 }
