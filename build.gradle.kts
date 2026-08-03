9a4baaf1e4648561adeb1c6ed7e3f8ae673fba48
Christian Klem <christian.klem@nav.no>
2026-08-03 13:07:33 +0200

    Diverse oppdateringer
     - Fiks noen sårbarheter
     - fjern unødvendig constraints
     - oppdater mock-oauth2-server

--- build.gradle.kts
+++ build.gradle.kts
@@ -78,39 +78,23 @@ dependencies {
 
     testImplementation("org.wiremock:wiremock-standalone:3.13.2")
     // Mock-oauth2-server
-    testImplementation("no.nav.security:mock-oauth2-server:4.0.1")
+    testImplementation("no.nav.security:mock-oauth2-server:5.0.2")
 
     constraints {
         implementation("com.fasterxml.jackson.core:jackson-core") {
-            version { require("2.22.0") }
-            because("versjoner < 2.21.1 har sårbarhet. inkludert i ktor-server-auth:3.4.0")
+            version { require("2.22.1") }
+            because("versjoner < 2.22.1 har sårbarhet. inkludert i ktor-server-auth:3.4.0")
         }
         implementation("tools.jackson.core:jackson-core") {
-            version { require("3.2.0") }
-            because("versjoner <= 3.1.0 har sårbarhet. inkludert i logstash-logback-encoder:9.0")
+            version { require("3.2.1") }
+            because("versjoner <= 3.2.0 har sårbarhet. inkludert i logstash-logback-encoder:9.0")
         }
         implementation("io.netty:netty-codec-http2") {
             version {
-                require("4.2.15.Final")
+                require("4.2.16.Final")
             }
             because(
-                "versjoner < 4.2.12.Final har sårbarhet. inkludert i ktor-server-netty-jvm:3.4.3",
-            )
-        }
-        testImplementation("org.bouncycastle:bcprov-jdk18on") {
-            version {
-                require("1.84")
-            }
-            because(
-                "versjoner < 1.84 har sårbarhet. inkludert i no.nav.security:mock-oauth2-server:3.0.1",
-            )
-        }
-        testImplementation("org.bouncycastle:bcpkix-jdk18on") {
-            version {
-                require("1.84")
-            }
-            because(
-                "versjoner < 1.84 har sårbarhet. inkludert i no.nav.security:mock-oauth2-server:3.0.1",
+                "versjoner < 4.2.16.Final har sårbarhet.",
             )
         }
     }
